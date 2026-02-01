eb86752b2cf424fa7fb97be6713767eb575b8ac6
mixxue <pison.mick1@gmail.com>
2026-02-01 22:28:52 +0700

    Update lab15_5.cpp

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int *,int *,int *,int *);
+void shuffle(int &,int &,int &,int &);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,12 +11,23 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(&a,&b,&c,&d);
+	    shuffle(a,b,c,d);
 	    cout << a << " " << b << " " << c << " " << d << endl;
 	}
 	
 	return 0;
 }
 
-//Do not modify the code above this line
-//Write definition of shuffle() using pointer after this line. 
+//Do not modify source code above this line
+void shuffle(int &a,int &b,int &c,int &d){
+	int arr[] = {a,b,c,d};
+	for(int i=0; i<2; i++){
+		int x = rand()%4;
+		int y = rand()%4;
+		swap(arr[x],arr[y]);
+	}
+	a = arr[0];
+	b = arr[1];
+	c = arr[2];
+	d = arr[3];
+}
